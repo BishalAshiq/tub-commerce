8e23aa40d119de5c9290e7ce8408923cc13493a0
Md. Ashiqul Islam <ashiqul.ba@pakiza.com>
2023-12-22 12:30:23 +0600

    first

---
+++ src/app/home/page.jsx
@@ -0,0 +1,15 @@
+import Navbar from "@/component/Navbar";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <h1 className='hello'>hello</h1>
+    </div>
+  );
+};
+
+export default page;
