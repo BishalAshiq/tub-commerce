36cef1448cd9a912d3c57f53eadcec3f60d64a26
Ashiqul Islam <bishaltheboss20@gmail.com>
2024-05-04 18:27:13 +0600

    recap 20 topics instead off 23

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const loading = () => {
     return (
-        <div>
-            <div>LOADING ............ !</div>
-           <div className="d-flex justify-content-center loading">
+        <div className="loading-pages">
+            <div className="loading-text">LOADING</div>
+           <div className="d-flex justify-content-center ">
                 <div className="spinner-grow text-info" role="status">
                 <span className="visually-hidden">Loading...</span>
                 </div>
